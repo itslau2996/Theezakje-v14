7900d3c4c493bdda206dbc81e0a878bcbdad6d8d
itslau2996 <wezellaurens@gmail.com>
2022-09-05 23:14:26 +0200

    Started working on help.js, and made user.js

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription(`Get all the commands`),
+    async execute(interaction) {
+        const HelpEmbed = new EmbedBuilder()
+            .setColor('DarkVividPink')
+            .setURL('https://discord.gg/xF7C3Cf37j')
+            .setTitle('Commands')
+            .addFields(
+                { name: 'Help', value: `This message!\nUsage: </help:1016090136418713690>`, inline: false},
+                { name: 'Topic', value: `Get a random subject to talk about!\nUsage: </topic:1015765607838400542>`, inline: false}
+            )
+        await interaction.reply({ embeds: [HelpEmbed]})
+    }
+}
---
+++ commands/user.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const moment = require('moment');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('user')
+        .setDescription(`Get a user's info`)
+        .addUserOption(option => option.setName('target').setDescription('Get the info of another user')),
+    async execute(interaction) {
+        const target = interaction.options.getUser('target')
+
+        if (target != null){
+            const user = interaction.options.getUser('target')
+            const response = new EmbedBuilder()
+                .setAuthor({ name: `${user.username}`, iconURL: user.displayAvatarURL({ dynamic: true })})
+                .addFields(
+                    { name: 'public profile', value: `ID: ${user.id}\n full username: ${user.tag}\n username: ${target.username}`, inline: false },
+                    { name: 'Discord user since:', value: `${moment(user.createdAt).format('MMMM Do YYYY, H:mm:ss')}`, inline: false},
+                )
+            await interaction.reply({ embeds: [response]})
+        } else {
+                const user = interaction.user
+                const response = new EmbedBuilder()
+                    .setAuthor({ name: `${user.tag}`, iconURL: user.displayAvatarURL({ dynamic: true})})
+                    .addFields(
+                        { name: 'public profile', value: `ID: ${user.id}\n full username: ${user.tag}`, inline: false },
+                        { name: 'Discord user since:', value: `${moment(user.createdAt).format('MMMM Do YYYY, H:mm:ss')}`, inline: false},
+                    )
+                await interaction.reply({ embeds: [response]})
+        }
+    }
+}
