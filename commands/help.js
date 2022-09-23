77a5198ccf2df1ff6b4b61601b5194761d6ecf1a
Laurens <93112857+itslau2996@users.noreply.github.com>
2022-09-23 14:29:08 +0200

    Help updated

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,8 @@ module.exports = {
             .setTitle('Commands')
             .addFields(
                 { name: 'Help', value: `This message!\nUsage: </help:1016090136418713690>`, inline: false},
-                { name: 'Topic', value: `Get a random subject to talk about!\nUsage: </topic:1015765607838400542>`, inline: false}
+                { name: 'Topic', value: `Get a random subject to talk about!\nUsage: </topic:1015765607838400542>`, inline: false},
+                { name: 'Server', value: `Get the info of the current server!\nUsage: </server:1022601385914667113>`, inline:false}
             )
         await interaction.reply({ embeds: [HelpEmbed]})
     }
